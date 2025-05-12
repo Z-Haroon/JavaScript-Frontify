c5187fc5788e8512ee636cccd2e53192cfc89379
Z-Haroon <workmailharoon@gmail.com>
2025-05-12 18:33:26 +0500

    Fully Functional, Responsive Category section Completed

--- organicStore/scripts/index.js
+++ organicStore/scripts/index.js
@@ -1,5 +1,51 @@
 import { inActiveOnLoad } from "./functions.js";
 
+// THIS IS CATEGORY SECTION SLIDER PROPERTIES
+let swiper = new Swiper(".category-mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true, // ✅ Infinite loop enabled
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".category-right__swipeBtn",
+    prevEl: ".category-left__swipeBtn",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    300: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    400: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    576: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },    
+    768: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    1440: {
+      slidesPerView: 8,
+      spaceBetween: 40,
+    }
+  }
+});
+
+
 (() => {
   // THIS FUNCTION FOR HIDE LOADING SCREEN
   inActiveOnLoad(".loading-screen");
